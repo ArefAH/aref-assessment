24c92f6592937d4385af1b1f9f0b82b2209440d9
ArefAH <arefabouhamdan@gmail.com>
2024-12-02 11:22:15 +0200

    Laravel <> Added routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix("/users")->group(function() {
+    Route::get("/", [UserController::class, "index"]);
+    Route::post("/", [UserController::class, "store"]);
+    Route::get("/{id}", [UserController::class, "show"]);
+    Route::put("/{id}", [UserController::class, "update"]);
+    Route::delete("/{id}", [UserController::class, "destroy"]);
+  });
+
+Route::prefix("/projects")->group(function() {
+    Route::get("/", [ProjectController::class, "index"]);
+    Route::post("/", [ProjectController::class, "store"]);
+    Route::get("/{id}", [ProjectController::class, "show"]);
+    Route::put("/{id}", [ProjectController::class, "update"]);
+    Route::delete("/{id}", [ProjectController::class, "destroy"]);
+  });
